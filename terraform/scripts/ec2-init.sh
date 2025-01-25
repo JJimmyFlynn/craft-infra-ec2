079ad98106bc5710ae691e020b5784ebf0f91f69
John Flynn <jjimmyflynn@gmail.com>
2025-01-24 20:00:52 -0500

    Update ec2 init script to pull in env vars

--- terraform/scripts/ec2-init.sh
+++ terraform/scripts/ec2-init.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 set -euo pipefail
 
-start_time=$SECONDS
-# TODO: replace hardcoded filename and site path
-# TODO: Remove timing profiling commands
-sitepath="/var/www/ec2.johnjflynn.net"
-filename="example-application-356310c.tar.gz"
+function set_env_variables() {
+    params=$(aws ssm get-parameters-by-path --path "$parameter_store_path")
 
-aws s3 cp "s3://${bucket_uri}/$filename" "$sitepath"
-tar -xzf "$sitepath/$filename" -C "$sitepath" --strip-components=1
-rm "$sitepath/$filename"
-chown -R www-data:www-data "$sitepath"
-chmod -R 775 "$sitepath/storage"
-rm "$sitepath/.env"
+    if (( $(echo "$params" | jq '.Parameters | length') = 0 )) ; then
+        return 1
+    fi
 
-# Set ENV Variables
+    jq -c '.Parameters[]' <<< "$params" | while read -r item; do
+      var_name=$(jq -cr '.Name' <<< "$item" |  awk -F'/' '{print $NF}')
+      var_value=$(jq -cr '.Value' <<< "$item")
 
-end_time=$SECONDS
-echo $((end_time - start_time)) > /script_time
+      echo "$var_name=$var_value" >> "$site_path/.env"
+    done
+}
+
+function main {
+  start_time=$SECONDS
+  # TODO: replace hardcoded filename and site path
+  # TODO: Remove timing profiling commands
+  site_path="/var/www/ec2.johnjflynn.net"
+  filename="example-application-356310c.tar.gz"
+  parameter_store_path="/example-application/dev"
+
+  aws s3 cp "s3://${bucket_uri}/$filename" "$site_path"
+  tar -xzf "$site_path/$filename" -C "$site_path" --strip-components=1
+  rm "$site_path/$filename"
+  chown -R www-data:www-data "$site_path"
+  chmod -R 775 "$site_path/storage"
+  rm "$site_path/.env"
+
+  # Set ENV Variables
+  set_env_variables
+
+  end_time=$SECONDS
+  echo $((end_time - start_time)) > /script_time
+}
+
+# Run script
+main
