e412aa3b348da4ec8c673611cd4b71fe01a794fe
John Flynn <jjimmyflynn@gmail.com>
2025-01-28 19:14:18 -0500

    Add redis to sample application, configure WAF

---
+++ terraform/environments/waf.tf
@@ -0,0 +1,198 @@
+resource "aws_wafv2_web_acl_association" "web_alb" {
+  resource_arn = aws_alb.default.arn
+  web_acl_arn  = aws_wafv2_web_acl.managed_rules.arn
+}
+
+resource "aws_wafv2_web_acl" "managed_rules" {
+  name  = module.this.id
+  description = "Managed rule groups to be associated with the ALB"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "aws-managed-rules"
+    sampled_requests_enabled   = false
+  }
+
+  rule {
+    name     = "aws-common-rules"
+    priority = 0
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-common-rules"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-kown-bad-inputs"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-known-bad-inputs"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-sql-common"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-sql-common"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-common-linux"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-common-linux"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-common-unix"
+    priority = 4
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesUnixRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-common-unix"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-common-php"
+    priority = 5
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesPHPRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-common-php"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-ip-reputation"
+    priority = 6
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-ip-reputation"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "aws-bot-protection"
+    priority = 7
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesBotControlRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "aws-bot-protection"
+      sampled_requests_enabled   = false
+    }
+  }
+}
+
+
